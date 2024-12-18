6891092cda0f90f555d88f594fda7dba65370c7c
Simon Quigley <simon@tsimonq2.net>
2024-12-17 20:58:30 -0600

    Rework distro_series further

--- cpp/lintian-ppa.cpp
+++ cpp/lintian-ppa.cpp
@@ -395,9 +395,11 @@ int main(int argc, char* argv[]) {
     auto ubuntu_opt = lp->distributions["ubuntu"];
     distribution ubuntu = ubuntu_opt.value();
     auto ds_opt = ubuntu.current_series;
-    auto current_series;
-    if (ds_opt)
-        current_series = ds_opt;
+    if (!ds_opt) {
+        std::cerr << "Failed to get current_series.\n";
+        return 1;
+    }
+    auto current_series = ds_opt;
 
     // Retrieve user and PPA
     auto user_opt = lp->people[args.user];
