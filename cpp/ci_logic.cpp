3f33edb4fa7bd256846aaf3d6755a0dd0afcb844
Simon Quigley <simon@tsimonq2.net>
2025-01-27 16:43:16 -0600

    Cut the future sleep in half

--- cpp/ci_logic.cpp
+++ cpp/ci_logic.cpp
@@ -697,7 +697,7 @@ std::string CiLogic::queue_pull_tarball(std::vector<std::shared_ptr<PackageConf>
                     encountered_items[r->package->name] = new_item;
                 }
             }));
-            std::this_thread::sleep_for(std::chrono::milliseconds(500));
+            std::this_thread::sleep_for(std::chrono::milliseconds(250));
         }
         for (auto& future : futures) future.get();
         msg = "Succeeded";
